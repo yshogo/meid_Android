cbb8b452ad983974027a38e5aed024fb7100169f
shogo.yamada <superman199323@gmail.com>
2017-07-06 11:00:12 +0900

    init commit

---
+++ app/src/main/java/com/example/shogoyamada/meid/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.shogoyamada.meid.activity;
+
+import android.os.Bundle;
+
+import com.example.shogoyamada.meid.R;
+import com.example.shogoyamada.meid.common.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
