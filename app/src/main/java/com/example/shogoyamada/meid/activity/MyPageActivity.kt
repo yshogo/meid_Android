b841f527b2c81d1768759528462d6469f1fd5662
shogo.yamada <superman199323@gmail.com>
2017-07-07 16:57:12 +0900

    スライドバーを出せるように修正

--- app/src/main/java/com/example/shogoyamada/meid/activity/MyPageActivity.kt
+++ app/src/main/java/com/example/shogoyamada/meid/activity/MyPageActivity.kt
@@ -1,6 +1,8 @@
 package com.example.shogoyamada.meid.activity
 
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import com.example.shogoyamada.meid.R
 import com.example.shogoyamada.meid.common.BaseActivity
 
@@ -8,8 +10,38 @@ import com.example.shogoyamada.meid.common.BaseActivity
 
 class MyPageActivity : BaseActivity() {
 
+
+    private var toggle: ActionBarDrawerToggle? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
+
+        //ツールバーの作成
+        createToolbar()
+    }
+
+    /**
+     * ツールバーを作成
+     */
+    private fun createToolbar(){
+        //ツールバーを初期化
+        val toolbar = findViewById(R.id.tool_bar) as android.support.v7.widget.Toolbar
+        setSupportActionBar(toolbar)
+
+        //トグルボタンを作成
+        val drawerLayout = findViewById(R.id.drawer_layout) as DrawerLayout
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name)
+        toggle!!.drawerArrowDrawable //ドロワーの使うか使わないかのオンオフ
+        drawerLayout.addDrawerListener(toggle!!)
+    }
+
+    /**
+     * レイアウト生成後にtoolbarを生成する
+     */
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        toggle!!.syncState()
     }
 }
